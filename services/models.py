96de199bf3e4d18608feea0fc68421a4fb12dc44
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-06-16 20:39:42 +0200

    feat: improving models and add to admin site

--- services/models.py
+++ services/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Service(models.Model):
-    name = models.CharField(max_length=100, unique=True, null=False)
+    name = models.CharField(max_length=100, unique=True)
     category = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
-    duration = models.IntegerField(help_text="Duration in minutes", null=False)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField(help_text="Duration in minutes")
 
     def __str__(self):
         return self.name
 
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100, unique=True, null=False)
-    last_name = models.CharField(max_length=100, unique=True, null=False)
-    email = models.EmailField(unique=True, null=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
     bio = models.TextField()
     services = models.ManyToManyField(Service, related_name='employees')
     
