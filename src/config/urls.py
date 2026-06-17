5c54b23329072c25212285269b9b5e92796685bc
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-06-17 16:09:45 +0200

    chore: initialize uv and reorganize project structure

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from services.views import home
+from src.services.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
